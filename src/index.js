1f996622e37d4d5283d7d1e370e04d8a5476b428
Sayfullah <sayfullah.jumoorty@live.com>
2023-04-27 00:12:50 +0200

    fix sprint 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./style.css";
 import Contact from "./views/contact";
 import { Register } from "./views/register";
-import TEST from "./views/test";
 import Home from "./views/home";
 import { Login } from "./views/login";
 import Competitions from "./views/competitions";
@@ -31,7 +30,6 @@ const App = () => {
       <div>
         <Route component={Contact} exact path="/contact" />
         <Route component={Register} exact path="/register" />
-        <Route component={TEST} exact path="/test" />
         <Route component={Home} exact path="/" />
         <Route component={Login} exact path="/login" />
         <Route component={Competitions} exact path="/competitions" />
